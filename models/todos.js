62c7029c2f6aa3ffa48e2bdb3b01f4d40650d27d
Your Name <Desi Suci Ramadhani>
2019-10-29 18:51:45 +0700

    Merubah nama table 'Todos' menjadi 'todos' pada folder models -> todos.js

--- models/todos.js
+++ models/todos.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Todos = sequelize.define('Todos', {
+  const Todos = sequelize.define('todos', {
     userId: DataTypes.INTEGER,
     title: DataTypes.STRING,
     description: DataTypes.STRING,
     dateActivity: DataTypes.DATE,
     completed: DataTypes.BOOLEAN
   }, {});
-  Todos.associate = function(models) {
+  Todos.associate = function (models) {
     // associations can be defined here
   };
   return Todos;
